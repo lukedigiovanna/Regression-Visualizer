98e11f7a727d7c4e95a4d8dee827939de5ef904f
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2021-05-16 11:41:31 -0400

    modifable functions

--- graphing.js
+++ graphing.js
@@ -2,71 +2,142 @@ const canvas = document.getElementById("graph-canvas");
 const ctx = canvas.getContext("2d");
 
 const width = canvas.width, height = canvas.height;
+const rect = canvas.getBoundingClientRect();
+const cw = rect.right - rect.left;
+const ch = rect.bottom - rect.top;
 
-canvas.addEventListener("click", function(e){
-    rect = canvas.getBoundingClientRect();
-    let cw = rect.right - rect.left;
-    let ch = rect.bottom - rect.top;
-    var x = e.clientX - rect.left,
-        y = ch - e.clientY + rect.top;
-    data.push([x / cw * graphWidth + minX, y / ch * graphHeight + minY])
-    updateTextArea();
-    drawGraph();
+var dragDown = false;
+canvas.addEventListener("mousedown", function(event) {
+    if (event.button == 1)
+        dragDown = true;
+});
+canvas.addEventListener("mouseup", function(event) {
+    dragDown = false;
+    if (event.button == 0) {
+        var x = event.clientX - rect.left,
+            y = ch - event.clientY + rect.top;
+        data.push([x / cw * graphWidth + minX, y / ch * graphHeight + minY])
+        updateTextArea();
+        drawGraph();
+    }
 });
 
-var graphWidth, graphHeight;
-var minX, maxX, minY, maxY;
+var lastX, lastY;
+canvas.addEventListener("mousemove", function(event) {
+   if (dragDown) {
+        let dx = (event.clientX - lastX) / cw * graphWidth;
+        let dy = (event.clientY - lastY) / ch * graphHeight;
+        translateView(-dx, dy);
+        drawGraph();
+   } 
+   lastX = event.clientX;
+   lastY = event.clientY;
+});
+
+canvas.addEventListener("wheel", function(event) {
+    if (event.deltaY < 0)
+        zoom(0.95);
+    else
+        zoom(1.05);
+});
+
+var graphWidth = 10, graphHeight = 10;
+var minX = -5, maxX = 5, minY = -5, maxY = 5;
+
+// fits the view of the graph to contain just the data
+function fitZoom() {
+    minX = data[0][0], maxX = minX;
+    minY = data[0][1], maxY = minY;
+    for (let i = 1; i < data.length; i++) {
+        if (data[i][0] < minX) minX = data[i][0];
+        else if (data[i][0] > maxX) maxX = data[i][0];
+        if (data[i][1] < minY) minY = data[i][1];
+        else if (data[i][1] > maxY) maxY = data[i][1];
+    }
+    graphWidth = maxX - minX, graphHeight = maxY - minY;
+    minX -= 0.1 * graphWidth;
+    maxX += 0.1 * graphWidth;
+    minY -= 0.1 * graphHeight;
+    maxY += 0.1 * graphHeight;
+    graphWidth = maxX - minX; 
+    graphHeight = maxY - minY;
+    drawGraph();
+}
+
+function translateView(dx, dy) {
+    maxX += dx;
+    minX += dx;
+    maxY += dy;
+    minY += dy;
+}
+
+function zoom(factor) {
+    graphWidth *= factor;
+    graphHeight *= factor;
+    let cx = (minX + maxX) / 2;
+    let cy = (minY + maxY) / 2;
+    minX = cx - graphWidth / 2;
+    maxX = cx + graphWidth / 2;
+    minY = cy - graphHeight / 2;
+    maxY = cy + graphHeight / 2;
+    drawGraph();
+}
+
+function xtgx(x) {
+    return (x - minX) / graphWidth * width;
+}
+
+function ytgy(y) {
+    return height - (y - minY) / graphHeight * height;
+}
 
 function drawGraph() {
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "lightgray";
     ctx.fillRect(0, 0, width, height);
     retrieveData(); // update the data from the list
+
+    let horizontalPlaces = Math.round(Math.log10(graphWidth));
+    let horizontalFactor = Math.pow(10, -(horizontalPlaces - 1));
+    let horizontalIncrement = Math.pow(10, horizontalPlaces - 1);
+    let startX = Math.round(minX * horizontalFactor) / horizontalFactor;
+    for (let x = startX; x < maxX; x += horizontalIncrement) {
+        if (x == 0) {
+            ctx.lineWidth = 4;
+            ctx.strokeStyle = "rgb(50,50,50)";
+        } else {
+            ctx.strokeStyle = "gray";
+            ctx.lineWidth = 2;
+        }
+        ctx.beginPath();
+        ctx.lineTo(xtgx(x), 0);
+        ctx.lineTo(xtgx(x), height);
+        ctx.stroke();
+    }
+    let verticalPlaces = Math.round(Math.log10(graphHeight));
+    let verticalFactor = Math.pow(10, -(verticalPlaces - 1));
+    let verticalIncrement = Math.pow(10, verticalPlaces - 1);
+    let startY = Math.round(minY * verticalFactor) / verticalFactor;
+    for (let y = startY; y < maxY; y += verticalIncrement) {
+        if (y == 0) {
+            ctx.lineWidth = 4;
+            ctx.strokeStyle = "rgb(50,50,50)";
+        } else {
+            ctx.strokeStyle = "gray";
+            ctx.lineWidth = 2;
+        }
+        ctx.beginPath();
+        ctx.lineTo(0, ytgy(y));
+        ctx.lineTo(width, ytgy(y));
+        ctx.stroke();
+    }
+
     if (data.length > 0) {
-        // find min/max x, y
-        minX = data[0][0], maxX = minX;
-        minY = data[0][1], maxY = minY;
-        for (let i = 1; i < data.length; i++) {
-            if (data[i][0] < minX) minX = data[i][0];
-            else if (data[i][0] > maxX) maxX = data[i][0];
-            if (data[i][1] < minY) minY = data[i][1];
-            else if (data[i][1] > maxY) maxY = data[i][1];
+        for (let i = 0; i < functions.length; i++) {
+            ctx.strokeStyle = functions[i].color;
+            functions[i].draw();
         }
-        graphWidth = maxX - minX, graphHeight = maxY - minY;
-        minX -= 0.1 * graphWidth;
-        maxX += 0.1 * graphWidth;
-        minY -= 0.1 * graphHeight;
-        maxY += 0.1 * graphHeight;
-        graphWidth = maxX - minX; 
-        graphHeight = maxY - minY;
 
-        // draw regression line
-        ctx.strokeStyle = "red";
-        ctx.beginPath();
-        ctx.lineTo(0, height - (predictLinear(minX) - minY) / graphHeight * height)
-        ctx.lineTo(width, height - (predictLinear(maxX) - minY) / graphHeight * height)
-        ctx.stroke();
-
-        // draw quadric regression line
-        ctx.strokeStyle = "blue";
-        ctx.beginPath();
-        for (let gx = 0; gx <= width; gx += 20) {
-            let x = gx / width * graphWidth + minX;
-            let y = predictQuadric(x);
-            ctx.lineTo(gx, height - (y - minY) / graphHeight * height);
-        }
-        ctx.stroke();
-
-        // draw nth term regression line
-        ctx.strokeStyle = "lime";
-        ctx.beginPath();
-        for (let gx = 0; gx <= width; gx += 5) {
-            let x = gx / width * graphWidth + minX;
-            let y = predictNthTerm(x);
-            ctx.lineTo(gx, height - (y - minY) / graphHeight * height);
-        }
-        ctx.stroke();
-
-        ctx.fillStyle = "lightgray";
+        ctx.fillStyle = "black";
         for (let i = 0; i < data.length; i++) {
             let gx = (data[i][0] - minX) / graphWidth * width;
             let gy = height - (data[i][1] - minY) / graphHeight * height;
--- regression.js
+++ regression.js
@@ -3,148 +3,144 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// linear vars
-var slope = 0;
-var intercept = 0;
+const functionsTag = document.getElementById("functions");
 
-// quadric vars
-var a = 0;
-var b = 0;
-var c = 0;
+var functions = []; 
 
-const n = 5;
-var nthTerm = []
-for (let i = 0; i < n; i++)
-    nthTerm.push(0);
-
-function stepLinearFit(learningRate = 0.0005) {
-    let slopeGradient = 0;
-    let interceptGradient = 0;
-    for (let i = 0; i < data.length; i++) {
-        interceptGradient += predictLinear(data[i][0]) - data[i][1];
-        slopeGradient += (predictLinear(data[i][0]) - data[i][1]) * data[i][0];
+function updateDisplay() {
+    let s = "<p class='label'>Functions:</p>";
+    for (let i = 0; i < functions.length; i++) {
+        s+="<p class='label'>"+functions[i].name+"</p>";
+        s+="<button onclick='functions["+i+"].fit();'>Train</button>";
+        s+="<button onclick='functions["+i+"].reset();'>Reset</button>";
+        s+="<p class='descriptor'>Cost: "+Math.round(functions[i].getCost()*100)/100+"</p>";
+        s+="<p class='descriptor'>"+functions[i].getEquation()+"</p>";
     }
-    slope -= slopeGradient * learningRate;
-    intercept -= interceptGradient * learningRate * 6;
-
-    drawGraph();
+    if (functions.length == 0) {
+        s += "<p class='label>None</p>";
+    }
+    functionsTag.innerHTML = s;
 }
 
-async function linearFit(learningRate = 0.0005) {
-    for (let i = 0; i < 2000; i++) {
-        stepLinearFit(learningRate);
-        await sleep(20);
-    }
+function addFunction() {
+    let order = parseInt(prompt("What degree?"))
+    new Regression(order+"th term", order);
+    updateDisplay();
 }
 
-function stepQuadricFit(learningRate = 0.000005) {
-    let aGradient = 0;
-    let bGradient = 0;
-    let cGradient = 0;
-    for (let i = 0; i < data.length; i++) {
-        let r = predictQuadric(data[i][0]) - data[i][1];
-        cGradient += r;
-        bGradient += r * data[i][0];
-        aGradient += r * data[i][0] * data[i][0];
-    }
-    a -= aGradient * learningRate;
-    b -= bGradient * learningRate * 4;
-    c -= cGradient * learningRate * 50;
+class Regression {
+    
+    constructor(name, numberOfTerms) {
+        this.color = 'black';
+        this.name = name;
+        this.terms = []
+        for (let i = 0; i < numberOfTerms; i++)
+            this.terms.push(0);
+        this.learningRate = 0.1;
+        this.learningRateIterations = 0;
+        this.lastCost = 0;
 
-    drawGraph();
-}
+        this.stopTraining = false;
 
-async function quadricFit(learningRate = 0.000005) {
-    for (let i = 0; i < 2000; i++) {
-        stepQuadricFit(learningRate);
-        await sleep(20);
+        functions.push(this);
     }
-}
 
-var nthLR = 1;
-var lastCost = 0;
-var lrIterations = 0;
-function stepNthTermFit() {
-    let gradient = [];
-    for (let i = 0; i < nthTerm.length; i++) {
-        gradient.push(0);
+    draw() {
+        ctx.beginPath();
+        for (let gx = 0; gx <= width; gx += 5) {
+            let x = gx / width * graphWidth + minX;
+            let y = this.predict(x);
+            ctx.lineTo(gx, ytgy(y));
+        }
+        ctx.stroke();
     }
-    for (let i = 0; i < data.length; i++) {
-        let r = predictNthTerm(data[i][0]) - data[i][1];
-        for (let j = 0; j < nthTerm.length; j++) {
-            gradient[j] += r * Math.pow(data[i][0], j);    
+
+    predict(x) {
+        let result = 0;
+        for (let i = 0; i < this.terms.length; i++) {
+            result += this.terms[i] * Math.pow(x, i)
+        }
+        return result;
+    }
+
+    getCost() {
+        let cost = 0;
+        for (let i = 0; i < data.length; i++) {
+            let residual = this.predict(data[i][0]) - data[i][1];
+            cost += residual * residual;
+        }
+        return cost;
+    }
+
+    stepFit() {
+        let gradient = [];
+        for (let i = 0; i < this.terms.length; i++) {
+            gradient.push(0);
+        }
+        for (let i = 0; i < data.length; i++) {
+            let r = this.predict(data[i][0]) - data[i][1];
+            for (let j = 0; j < this.terms.length; j++) {
+                gradient[j] += r * Math.pow(data[i][0], j);    
+            }
+        }
+    
+        for (let j = 0; j < this.terms.length; j++) {
+            this.terms[j] -= gradient[j] * this.learningRate * Math.pow(50, this.terms.length - 1 - j);
+        }
+    
+        let cost = this.getCost();
+        if (this.learningRateIterations > 0 && Math.abs(cost - this.lastCost) > 1000) {
+            this.learningRate /= 10;
+            for (let i = 0; i < this.terms.length; i++)
+                this.terms[i] = 0;
+            this.learningRateIterations = 0;
+        }
+        else {
+            this.learningRateIterations++;
+            this.lastCost = cost;
+        }
+
+        // find the l2 norm of the gradient
+        let l2 = 0;
+        for (let i = 0; i < gradient.length; i++) {
+            l2 += gradient[i] * gradient[i];
+        }
+        if (l2 < 0.0000001)
+            this.stopTraining = true; // when the gradient is so small, it's negligible
+
+        drawGraph();
+    }
+
+    async fit() {
+        this.stopTraining = false;
+        while (!this.stopTraining) {
+            this.stepFit();
+            await sleep(20);
         }
     }
 
-    for (let j = 0; j < nthTerm.length; j++) {
-        nthTerm[j] -= gradient[j] * nthLR * Math.pow(50, nthTerm.length - 1 - j);
+    getEquation() {
+        let s = "y = ";
+        for (let i = 0; i < this.terms.length; i++) {
+            s += Math.round(this.terms[i] * 100) / 100 + "x^" + i;
+            if (i < this.terms.length - 1)
+                s += " + ";
+        }
+        return s;
     }
 
-    let cost = getNthTermCost();
-    if (lrIterations > 0 && Math.abs(cost - lastCost) > 1000) {
-        nthLR /= 10;
-        for (let i = 0; i < nthTerm.length; i++)
-            nthTerm[i] = 0;
-        lrIterations = 0;
-    }
-    else {
-        lrIterations++;
-        lastCost = cost;
-    }
-    drawGraph();
-}
-
-async function nthTermFit() {
-    for (let i = 0; i < 2000; i++) {
-        for (let j = 0; j < 3000; j++)
-        stepNthTermFit();
-        await sleep(20);
+    reset() {
+        this.learningRate = 0.1;
+        for (let i = 0; i < this.terms.length; i++) {
+            this.terms[i] = 0;
+        }
+        this.learningRateIterations = 0;
+        this.stopTraining = false;
+        drawGraph();
     }
 }
 
-function predictLinear(x) {
-    return slope * x + intercept;
-}
-
-function predictQuadric(x) {
-    return a * x * x + b * x + c;
-}
-
-function predictNthTerm(x) {
-    let result = 0;
-    for (let i = 0; i < nthTerm.length; i++) {
-        result += nthTerm[i] * Math.pow(x, i)
-    }
-    return result;
-}
-
-function getLinearCost() {
-    let cost = 0;
-    for (let i = 0; i < data.length; i++) {
-        let residual = predictLinear(data[i][0]) - data[i][1];
-        cost += residual * residual;
-    }
-    return cost;
-}
-
-function getQuadricCost() {
-    let cost = 0;
-    for (let i = 0; i < data.length; i++) {
-        let residual = predictQuadric(data[i][0]) - data[i][1];
-        cost += residual * residual;
-    }
-    return cost;
-}
-
-function getNthTermCost() {
-    let cost = 0;
-    for (let i = 0; i < data.length; i++) {
-        let residual = predictNthTerm(data[i][0]) - data[i][1];
-        cost += residual * residual;
-    }
-    return cost;
-}
-
+/*
 const linearCostP = document.getElementById("linear-cost");
 const slopeP = document.getElementById("slope");
 const interceptP = document.getElementById("intercept");
@@ -158,21 +154,16 @@ const nthTermCostP = document.getElementById("nth-cost");
 const nthEquationP = document.getElementById("nth-equation");
 
 function updateDisplay() {
-    linearCostP.innerHTML = Math.round(getLinearCost() * 100) / 100;
-    slopeP.innerHTML = Math.round(slope * 100) / 100;
-    interceptP.innerHTML = Math.round(intercept * 100) / 100;
+    linearCostP.innerHTML = Math.round(linearRegression.getCost() * 100) / 100;
+    slopeP.innerHTML = Math.round(linearRegression.terms[1] * 100) / 100;
+    interceptP.innerHTML = Math.round(linearRegression.terms[0] * 100) / 100;
 
-    quadricCostP.innerHTML = Math.round(getQuadricCost() * 100) / 100;
-    aP.innerHTML = Math.round(a * 100) / 100;
-    bP.innerHTML = Math.round(b * 100) / 100;
-    cP.innerHTML = Math.round(c * 100) / 100;
+    quadricCostP.innerHTML = Math.round(quadricRegression.getCost() * 100) / 100;
+    aP.innerHTML = Math.round(quadricRegression.terms[2] * 100) / 100;
+    bP.innerHTML = Math.round(quadricRegression.terms[1] * 100) / 100;
+    cP.innerHTML = Math.round(quadricRegression.terms[0] * 100) / 100;
 
-    nthTermCostP.innerHTML = Math.round(getNthTermCost() * 100) / 100;
-    let s = "y = ";
-    for (let i = 0; i < nthTerm.length; i++) {
-        s += Math.round(nthTerm[i] * 100) / 100 + "x^" + i;
-        if (i < nthTerm.length - 1)
-            s += " + ";
-    }
-    nthEquationP.innerHTML = s;
+    nthTermCostP.innerHTML = Math.round(nthTermRegression.getCost() * 100) / 100;
+    nthEquationP.innerHTML = nthTermRegression.getEquation();
 }
+*/
